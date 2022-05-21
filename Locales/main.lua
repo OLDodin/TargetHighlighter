18d2502d4f5de1566c8b0c4af806ceab21390cbd
OLDodin <odin_sasa@mail.ru>
2022-05-21 17:35:05 +0300

    new localization system

--- Locales/main.lua
+++ Locales/main.lua
@@ -1,32 +1,15 @@
 Global("Locales", {})
 
+local localeGroup = common.GetAddonRelatedTextGroup(common.GetLocalization()) or common.GetAddonRelatedTextGroup("eng")
+
 function getLocale()
-	return Locales[common.GetLocalization()] or Locales["eng"]
+	return setmetatable(Locales, 
+		{__index = function(t,k) 
+			if localeGroup:HasText(k) then
+				return localeGroup:GetText(k) 
+			end
+		end
+		}
+	)
 end
-
---------------------------------------------------------------------------------
--- Russian
---------------------------------------------------------------------------------
-
-Locales["rus"]={}
-Locales["rus"]["line1"]="Кого: "
-Locales["rus"]["colorMode1Btn"]="Цвет: "
-Locales["rus"]["colorMode2Btn"]="Цвет: "
-Locales["rus"]["saveBtn"]="Сохранить"
-Locales["rus"]["header"]="Подсветка цели"
-Locales["rus"]["useMode1"]="Использовать обводку"
-Locales["rus"]["useMode2"]="Использовать заливку"
-Locales["rus"]["disableSystemHighlight"]="Выкл. стандарную подсветку"
-Locales["rus"]["setColorButton"]="OK"
-Locales["rus"]["red"]="Красный"
-Locales["rus"]["green"]="Зелёный"
-Locales["rus"]["blue"]="Синий"
-Locales["rus"]["alpha"]="Прозрач."
-Locales["rus"]["headerColor"]="Выберите цвет"
-
---------------------------------------------------------------------------------
--- English
---------------------------------------------------------------------------------
-
-Locales["eng"]={}
 
